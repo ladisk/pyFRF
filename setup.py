7f84cde1801550f5f2b4e307beee98bf21b7051c
Janko Slavič <janko.slavic@gmail.com>
2016-11-03 11:47:14 +0100

    Corrected setup.py - now works as it should:)

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ For a showcase see: https://github.com/openmodal/pyFRF/blob/master/Showcase%20py
 #from distutils.core import setup, Extension
 from setuptools import setup, Extension
 setup(name='pyFRF',
-      version='0.1.2',
+      version='0.2.1',
       author='Janko Slavič',
       author_email='janko.slavic@fs.uni-lj.si',
       description='Frequency response function as used in structural dynamics.',
       url='https://github.com/openmodal/pyFRF',
-      py_modules=['pyFRF'],
+      py_modules=['pyFRF','fft_tools'],
       #ext_modules=[Extension('lvm_read', ['data/short.lvm'])],
       long_description=desc,
       requires=['numpy']

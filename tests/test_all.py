3656ef246d13c731f071c6fbc7bbb815cc0215c2
Janko Slavič <janko.slavic@gmail.com>
2017-03-01 17:16:13 +0100

    pyFRF-3 ENG: add FRF support for accelerance, mobility, receptance

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,6 +3,9 @@ Unit test for pyFRF.py
 """
 import numpy as np
 from pyFRF import FRF
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
 
 def test_synthetic():
     C = 0.5 + 0.1j  # modal constant

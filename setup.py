9df4f57f15d80cbfdf7e48ab5fee5bb19bb61d67
Janko Slavič <janko.slavic@gmail.com>
2023-10-03 15:39:18 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,23 +1,47 @@
-desc = """\
-pyFRF
-======
-
-Frequency response function as used in structural dynamics.
------------------------------------------------------------
-
-For a showcase see: https://github.com/ladisk/pyFRF/blob/main/Showcase%20pyFRF.ipynb
-"""
-
-#from distutils.core import setup, Extension
+import os
+import re
 from setuptools import setup
-setup(name='pyFRF',
-      version='0.41',
-      author='Janko Slavič et al.',
+
+regexp = re.compile(r'.*__version__ = [\'\"](.*?)[\'\"]', re.S)
+
+base_path = os.path.dirname(__file__)
+
+init_file = os.path.join(base_path, 'pyFRF.py')
+with open(init_file, 'r') as f:
+    module_content = f.read()
+
+    match = regexp.match(module_content)
+    if match:
+        version = match.group(1)
+    else:
+        raise RuntimeError(
+            'Cannot find __version__ in {}'.format(init_file))
+
+
+with open(os.path.join(base_path, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def parse_requirements(filename):
+    ''' Load requirements from a pip requirements file '''
+    with open(filename, 'r') as fd:
+        lines = []
+        for line in fd:
+            line.strip()
+            if line and not line.startswith("#"):
+                lines.append(line)
+    return lines
+
+requirements = parse_requirements('requirements.txt')
+
+setup(name='pyuff',
+      version=version,
+      author='Janko Slavič, Luka Novak, Martin Česnik, et al.',
       author_email='janko.slavic@fs.uni-lj.si',
       description='Frequency response function as used in structural dynamics.',
       url='https://github.com/ladisk/pyFRF',
       py_modules=['pyFRF','fft_tools'],
-      #ext_modules=[Extension('lvm_read', ['data/short.lvm'])],
-      long_description=desc,
-      install_requires=['numpy']
+      long_description=long_description,
+      install_requires=requirements,
+      keywords='FRF, MIMO, SIMO, ODS',
       )
